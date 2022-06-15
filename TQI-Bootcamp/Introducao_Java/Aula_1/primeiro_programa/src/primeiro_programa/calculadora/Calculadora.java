824b761645a06da0997bff1e0a721b803c0481ea
PedroCamargos <pedropaulosilvacamargos@gmail.com>
2022-06-15 19:17:49 -0300

    Criação de Calculadora

---
+++ TQI-Bootcamp/Introducao_Java/Aula_1/primeiro_programa/src/primeiro_programa/calculadora/Calculadora.java
@@ -0,0 +1,48 @@
+package primeiro_programa.calculadora;
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		float a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		float soma = soma(a, b);
+		float subtracao = subtracao(a, b);
+		float multiplicacao = multiplicacao(a, b);
+		float divisao = divisao(a, b);
+		
+		System.out.println("A soma dos valores é: " + soma);
+		
+		System.out.println("A subtração dos valores é: " + subtracao);
+		
+		System.out.println("A multiplicação dos valores é: " + multiplicacao);
+		
+		System.out.println("A divisão dos valores é: " + divisao);
+		
+	}
+	
+	public static float soma(float a, float b) {
+		return a + b;
+	}
+	
+	public static float subtracao(float a, float b) {
+		return a - b;
+	}
+	
+	public static float multiplicacao(float a, float b) {
+		return a * b;
+	}	
+	
+	public static float divisao(float a, float b) {
+		return a / b;
+	}	
+	
+}	
